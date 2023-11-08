c0a8e5f94eab6df9224f9b4dfb413a9908bf85ab
Vishnu <vinventor1999@gmail.com>
2023-11-09 04:36:20 +0530

    Related pages of checkout done

--- WebShopping/helpers/userhelpers.js
+++ WebShopping/helpers/userhelpers.js
@@ -249,7 +249,8 @@ module.exports = {
             paymentMethod:order['payment-method'],
             products:products,
             totalAmount:total,
-            status:status
+            status:status,
+            date:new Date()
          }
 
          db.get().collection(collection.ORDER_COLLECTION).insertOne(orderObj).then((response) => {
@@ -265,6 +266,50 @@ module.exports = {
          let cart = await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
          console.log(cart)
          resolve(cart.products)
+      })
+   },
+
+   getUserOrders:(userId) => {
+      return new Promise(async(resolve,reject) => {
+         console.log(userId)
+         let orders = await db.get().collection(collection.ORDER_COLLECTION)
+         .find({userId:objectId(userId)}).toArray()
+         console.log(orders)
+         resolve(orders)
+      })
+   },
+
+   getOrderProducts:(orderId) => {
+      return new Promise(async(resolve,reject) => {
+         let orderItems = await db.get().collection(collection.ORDER_COLLECTION).aggregate([
+            {
+               $match:{_id:objectId(orderId)}
+            },
+            {
+               $unwind:'$products'
+            },
+            {
+               $project:{
+                  item:'$products.item',
+                  quantity:'$products.quantity'
+               }
+            },
+            {
+               $lookup:{
+                  from:collection.PRODUCT_COLLECTION,
+                  localField:'item',
+                  foreignField:'_id',
+                  as:'product'
+               }
+            },
+            {
+               $project:{
+                  item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
+               }
+            }
+         ]).toArray()
+         console.log(orderItems)
+         resolve(orderItems)
       })
    }
 
